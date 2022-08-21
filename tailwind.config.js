73dbe7294a0f78a359e62989a2d9a9460b5f5c50
kugyu10 <kugyu10@gmail.com>
2022-08-21 14:50:15 +0900

    install ESLint

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}", //追加
+    "./src/components/**/*.{js,ts,jsx,tsx}", //追加
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
