4398e3c39ccbaf55d906777693050b837d9c9dc5
kugyu10 <kugyu10@gmail.com>
2022-09-25 15:52:10 +0900

    add organisms/Footer

---
+++ src/components/organisms/Footer/index.stories.tsx
@@ -0,0 +1,6 @@
+import { ComponentMeta } from '@storybook/react'
+import Footer from './index'
+
+export default { tilte: 'Organisms/Footer' } as ComponentMeta<typeof Footer>
+
+export const Standard = () => <Footer />
